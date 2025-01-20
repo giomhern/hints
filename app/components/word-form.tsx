b074f5866c46c8ad82150ec0c8e79fdb2a5af7eb
Giovanni Maya <giovannimhern@uchicago.edu>
2025-01-19 19:42:39 -0600

    initial commit

---
+++ app/components/word-form.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const WordForm = () => {
+  return (
+    <form className="space-y-4">
+      <div>
+        
+      </div>
+    </form>
+  );
+};
+
+export default WordForm;
