6fe3597050a98ea8e06e260c7db662d5cced944c
Giovanni Maya <giovannimhern@uchicago.edu>
2025-01-20 17:08:38 -0600

    updated actions to support guesses

--- app/actions.ts
+++ app/actions.ts
@@ -1,5 +1,4 @@
 "use server";
-
 import { supabase } from "./utils";
 
 export async function createGame(word: string) {
@@ -10,7 +9,7 @@ export async function createGame(word: string) {
   try {
     const { data, error } = await supabase
       .from("games")
-      .insert({ word: word })
+      .insert({ word: word.toUpperCase() })
       .select("public_id")
       .single();
 
@@ -19,10 +18,54 @@ export async function createGame(word: string) {
     }
 
     console.log("Redirecting to:", `/game/${data.public_id}`);
-    return data 
+    return data;
   } catch (error) {
     throw new Error(`Error creating game: ${error}`);
   }
 }
 
+export async function fetchGame(publicId: string) {
+  try {
+    const { data, error } = await supabase
+      .from("games")
+      .select("words, guesses, finished")
+      .eq("public_id", publicId)
+      .single();
 
+    if (error) {
+      throw new Error(`Error fetching game: ${error.message}`);
+    }
+    return data;
+  } catch (error) {
+    throw new Error(`Error fetching game: ${error}`);
+  }
+}
+
+export async function makeGuess(publicId: string, guess: string) {
+  try {
+    const { data, error } = await supabase
+      .from("games")
+      .select("guesses, word,finished")
+      .eq("public_id", publicId)
+      .single();
+
+    if (error) {
+      throw new Error(`Error fetching game: ${error.message}`);
+    }
+
+    const isCorrect = data.word.toUpperCase() === guess.toUpperCase();
+
+    const { error: updateError } = await supabase.from("games").update({
+      guesses: [...data.guesses, guess],
+      finished: isCorrect,
+    });
+
+    if (updateError) {
+      throw new Error(`Error updating game: ${updateError.message}`);
+    }
+
+    return { isCorrect, guesses: [...data.guesses, guess] };
+  } catch (error) {
+    throw new Error(`Error making guess: ${error}`);
+  }
+}
