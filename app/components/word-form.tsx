b8374366093e310b105573ba3f0fcdf260a032d0
Giovanni Maya <giovannimhern@uchicago.edu>
2025-01-19 19:53:19 -0600

    creating initial word form

--- app/components/word-form.tsx
+++ app/components/word-form.tsx
@@ -3,10 +3,26 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 const WordForm = () => {
+  const [word, setWord] = useState("");
+  const router = useRouter();
+
   return (
     <form className="space-y-4">
       <div>
-        
+        <label
+          htmlFor="word"
+          className="block text-lg font-bold text-gray-700"
+          style={{ fontFamily: "var(--font-outfit)" }}
+        >
+          Enter a word or phrase:
+        </label>
+        <input
+          type="text"
+          id="word"
+          className="w-full p-2"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+        />
       </div>
     </form>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import WordForm from "./components/word-form";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen">
       <h1
-        className="text-5xl font-bold"
+        className="text-5xl font-bold text-gray-700"
         style={{ fontFamily: "var(--font-jersey)" }}
       >
         hints
