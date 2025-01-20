53e2f3d8f793f6d58706c2aa74e89ea99266dccf
Giovanni Maya <giovannimhern@uchicago.edu>
2025-01-19 21:00:34 -0600

    word form completed yay

--- app/components/word-form.tsx
+++ app/components/word-form.tsx
@@ -8,7 +8,7 @@ const WordForm = () => {
 
   return (
     <form className="space-y-4">
-      <div>
+      <div className="space-y-2">
         <label
           htmlFor="word"
           className="block text-lg font-bold text-gray-700"
@@ -19,11 +19,20 @@ const WordForm = () => {
         <input
           type="text"
           id="word"
-          className="w-full p-2"
+          className="w-full py-2 px-4 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-transparent border border-gray-300 rounded-md"
           value={word}
+          style={{ fontFamily: "var(--font-outfit)" }}
           onChange={(e) => setWord(e.target.value)}
+          placeholder="e.g. hello world"
         />
       </div>
+      <button
+        type="submit"
+        style={{ fontFamily: "var(--font-jersey)" }}
+        className="px-4 py-2 bg-gray-800 text-white text-lg rounded hover:bg-gray-700 focus:outline-none"
+      >
+        Create Game
+      </button>
     </form>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import WordForm from "./components/word-form";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <h1
-        className="text-5xl font-bold text-gray-700"
-        style={{ fontFamily: "var(--font-jersey)" }}
-      >
-        hints
-      </h1>
-      <WordForm />
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-1/3">
+        <h1
+          className="text-5xl font-bold text-gray-700 text-center"
+          style={{ fontFamily: "var(--font-jersey)" }}
+        >
+          hints
+        </h1>
+        <WordForm />
+      </div>
     </div>
   );
 }
